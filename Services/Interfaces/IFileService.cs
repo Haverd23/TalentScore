436b4d57486b1a484346fb26b0555ad18971afd1
João <GabrielAlvees616@gmail.com>
2026-09-01 12:43:09 -0300

    Criado Interface e Implementado a lógica do Service File

---
+++ Services/Interfaces/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace TalentScore.Services.Interfaces
+{
+    public interface IFileService
+    {
+        public void ValidateFile(IFormFile file);
+        public string GetContentType(IFormFile file);
+    }
+}
